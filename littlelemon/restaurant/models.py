42b995fe81fd15ed603b10b1b90ef54360d7406e
Vinicius Silva Teixeira <viniciusst@gmail.com>
2024-12-14 21:39:22 -0500

    Add Models and Migrations

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    noOfGuests = models.IntegerField()
+    bookingDate = models.DateTimeField(db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
+    inventory = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
