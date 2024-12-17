2d0353983f5496822949be5fc9e033a74473fe97
Vinicius Silva Teixeira <viniciusst@gmail.com>
2024-12-17 14:55:50 -0500

    Add Serializer - API

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Booking
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
